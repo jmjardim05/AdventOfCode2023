4fb577be19e6ed17e20a9d24368b8f6ef1c0b545
joao.jardim <joao.jardim@pglbr.com.br>
2024-01-09 16:59:21 -0300

    Day 1 Complete

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -9,7 +9,7 @@ const readInputFile = require("../readInput");
 
 const calibrationValues = readInputFile("Day 1");
 
-console.time("Day 1");
+console.time("Day 1 - Part 1");
 let sum = 0;
 calibrationValues.forEach(
    value => {
@@ -21,5 +21,38 @@ calibrationValues.forEach(
    }
 );
 
-console.log(`Day 1 - Result is ${sum}`);
-console.timeEnd("Day 1");
+console.log(`Day 1 - P - Result is ${sum}`);
+console.timeEnd("Day 1 - Part 1");
+
+/*
+   Day 1 - Part 2
+   Need to consider the spelled out numbers now (eg: one = 1, two = 2)
+*/
+
+console.time("Day 1 - Part 2");
+sum = 0;
+const numbers = ["\\d","one","two","three","four","five","six","seven","eight","nine"];
+
+const getDigits = value => {
+   let digits = [];   
+   numbers.forEach(element => {
+      const expr = new RegExp(element, "g");
+      digits = [...digits, ...value.matchAll(expr)];
+   });
+   return digits.sort((a, b) => a.index - b.index);
+}
+
+calibrationValues.forEach(
+   value => {
+      const digits = getDigits(value);
+      const n1 = numbers.indexOf(digits[0][0]);
+      const digit1 = (n1 >= 1) ? n1.toString() : digits[0][0];
+      const n2 = numbers.indexOf(digits[digits.length - 1][0]);
+      const digit2 = (n2 >= 0) ? n2.toString() : digits[digits.length - 1][0];
+      const calibrationVal = digit1 + digit2;
+      sum += Number.parseInt(calibrationVal);
+   }
+);
+
+console.log(`Day 1 - Part 2 - Result is ${sum}`);
+console.timeEnd("Day 1 - Part 2");
