e84d882b1504b7699f99ca1d197899066c87100a
jmjardim05@hotmail.com <Joo>
2024-01-17 16:52:16 -0300

    Day 4 Completed

--- Day 4/day4.js
+++ Day 4/day4.js
@@ -50,29 +50,25 @@ console.timeEnd("Day 4 - Part 1")
 /*
     Day 4 - Part 2
 */
-let sum = 0;
-const solvePart2 = (cardIndex, copies) => {
-    let copiesProcessed = 0;
-    scratchCards.forEach((card, thisCard) => {
-        if (cardIndex > thisCard)
-            return
 
+const solvePart2 = (cardIndex, copies) => {
+    let sum = 0
+    for (let thisCard = cardIndex; thisCard < (cardIndex + copies); thisCard++) {        
+        const card = scratchCards[thisCard]
         const lists = card.split("|");
         const winningNumbers = getNumbers(lists[0])
         const yourNumbers = getNumbers(lists[1])
 
-        const copiesWon = yourNumbers.filter(number => winningNumbers.includes(number)).length
+        const copiesWon = yourNumbers.filter(number => winningNumbers.includes(number)).length        
         sum++
-        copiesProcessed++
-        if (copiesWon === 0 || copiesProcessed === copies)
-            cardIndex = scratchCards.length                
-        if (copiesProcessed === copies)
-            return        
-        solvePart2(thisCard + 1, copiesWon)        
-    })
+        if (copiesWon === 0) {
+            continue
+        }        
+        sum += solvePart2(thisCard + 1, copiesWon)        
+    }
     return sum
 }
 
 console.time("Day 4 - Part 2")
-console.log(solvePart2(0, 6))
+console.log(`Day 4 - Part 2 - Result is ${solvePart2(0, scratchCards.length)}`)
 console.timeEnd("Day 4 - Part 2")
